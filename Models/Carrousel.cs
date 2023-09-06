6540178f98bef4a3f72020dbe946125376a06919
VaneCarreras <vanecarreras91@gmail.com>
2023-09-06 19:42:51 -0300

    Validaciones del carrousel

--- Models/Carrousel.cs
+++ Models/Carrousel.cs
@@ -6,8 +6,12 @@ public class Carrousel
 {
     [Key]
     public int Id { get; set; }
+
     public byte Imagen { get; set; }
-    [StringLength(200)]
+    
+    [StringLength(200, MinimumLength = 5,
+        ErrorMessage = "La descripción debe tener como minimo 5 y como maximo 200 caracteres")]
+    [DataType(DataType.Text)]
     public string? Descripcion { get; set; }
 
 }
