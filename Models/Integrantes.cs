d336388a83083ee7cb08c1a18878b146ce9428c4
Lucas Orsini <86387404+LJorsini@users.noreply.github.com>
2023-09-07 20:25:01 -0300

    DataAnnotation Integrantes (#18)

--- Models/Integrantes.cs
+++ Models/Integrantes.cs
@@ -4,12 +4,20 @@ namespace ArchivoHistoricoApi.Models;
 
 public class Integrantes
 {
+   
+    
     [Key]
     public int Id { get; set; }
+
+    [Required]
     public byte Foto { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
     [StringLength(200)]
     public string? Nombre { get; set; }
-    [StringLength(150)]
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [StringLength(200)]
     public string? Cargo { get; set; }
 
 }
