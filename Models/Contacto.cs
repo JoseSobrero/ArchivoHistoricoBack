f009e1ea7ede828a25074e916479653485497850
Nicolás Maidana <102559621+StJimmy130@users.noreply.github.com>
2023-09-07 20:24:31 -0300

    DataAnnotationContacto (#17)

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -6,12 +6,25 @@ public class Contacto
 {
     [Key]
     public int Id { get; set; }
+    
+    
     [StringLength(150)]
+    [Required(ErrorMessage ="Este campo es obligatorio.")]
     public string? Telefono { get; set; }
+    
+    
     [StringLength(200)]
+    [Required(ErrorMessage ="Este campo es obligatorio.")]
     public string? Email { get; set; }
-    public string? Domicilio { get; set; }
+    
+    
     [StringLength(200)]
+    [Required(ErrorMessage ="Este campo es obligatorio.")]
+    public string? Domicilio { get; set; }
+    
+    
+    [StringLength(200)]
+    [Required(ErrorMessage ="Este campo es obligatorio.")]
     public string? EmailForm { get; set; }
 
 }
