bb29ce1754736ec469d11b0b16720bb3f3bc628d
ByDavito <105891021+ByDavito@users.noreply.github.com>
2023-09-27 19:01:50 -0300

    Controlador libro (#22)
    * DataAnnotation Libros
    
    * DataAnotation libros (Corregido)
    
    ---------
    
    Co-authored-by: José Sobrero <josesobrero25@hotmail.com>

--- Models/Libros.cs
+++ Models/Libros.cs
@@ -6,17 +6,21 @@ public class Libros
 {
     [Key]
     public int Id { get; set; }
+  
     [Required (ErrorMessage ="Este campo es obligatorio")]
     [StringLength (150)]
     [DataType(DataType.Text)]
     public string? Nombre { get; set; }
+  
     [Required (ErrorMessage ="Este campo es obligatorio")]
     [StringLength(300)]
     [DataType(DataType.Text)]
     public string? Resenia { get; set; }
+  
     [Required (ErrorMessage ="Este campo es obligatorio")]
     [StringLength (150)]
     [DataType(DataType.Text)]
     public string? DondeConseguirlo { get; set; }
+    
     public byte Foto { get; set; }
 }
