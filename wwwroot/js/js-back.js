7be24d914da0af796cb80d4b61090363d7e497b2
ByDavito <daviracca02@gmail.com>
2023-11-15 00:32:14 -0300

    js 1.5

--- wwwroot/js/js-back.js
+++ wwwroot/js/js-back.js
@@ -59,7 +59,10 @@ function traerLibros() {
 
 function mostrarLibros(data) {
     const contenedorCards = document.getElementById('contenedorCards');
-    
+    const elementoEspecial = document.getElementById('btn-agregar');
+    contenedorCards.innerHTML = '';
+
+    contenedorCards.appendChild(elementoEspecial);
 
     data.forEach(element => {
         let col = document.createElement('div');
@@ -69,7 +72,7 @@ function mostrarLibros(data) {
         card.classList.add('card-items');
 
         let overlay = document.createElement('div');
-        overlay.classList.add('overlay');
+        overlay.classList.add('overlay-card');
 
         let nombre = document.createElement('h3');
         nombre.innerText = element.nombre;
@@ -84,12 +87,14 @@ function mostrarLibros(data) {
         btnContainer.classList.add('btn-container');
 
         let btn1 = document.createElement('button');
-        btn1.innerText = 'Botón 1';
+        btn1.innerText = 'Editar';
         btn1.classList.add('btn');
+        btn1.setAttribute('onclick', `buscarLibro(${element.id})`);
     
         let btn2 = document.createElement('button');
-        btn2.innerText = 'Botón 2';
+        btn2.innerText = 'Eliminar';
         btn2.classList.add('btn');
+        btn2.setAttribute('onclick', `validacionEliminar(${element.id})`);
 
 
         card.appendChild(overlay);
@@ -99,9 +104,9 @@ function mostrarLibros(data) {
         overlay.appendChild(btnContainer);
         btnContainer.appendChild(btn1);
         btnContainer.appendChild(btn2);
-
         contenedorCards.appendChild(card);
     });
+    
 }
 
 
@@ -139,4 +144,66 @@ body: JSON.stringify(nuevoLibro)
     traerLibros();
 })
 .catch(error => console.error("no se pudo agregar el libro", error));
+}
+
+function validacionEliminar(id) {
+    var siEliminar = confirm("Desea eliminar el elemento?");
+    if (siEliminar == true) {
+        Eliminar(id)
+    }
+}
+
+function Eliminar(id) {
+    fetch(`https://localhost:7153/api/Libros/${id}`,
+    
+    {    method: 'DELETE',
+    })
+    .then(() => { traerLibros(); })
+    .catch(error=> console.error("No se pudo borrar el elemento"))
+    
+}
+
+function buscarLibro(id) {
+    fetch(`https://localhost:7153/api/Libros/${id}`, {
+        method: 'GET'
+    })
+        .then(respuesta => respuesta.json())
+        .then(data => {
+            document.getElementById('idEditar').value = data.id;
+            document.getElementById('nombreEditar').value = data.nombre;
+            document.getElementById('reseniaEditar').value = data.resenia;
+            document.getElementById('dondeConseguirloEditar').value = data.dondeConseguirlo;
+            $('#editarLibro').modal('show');
+        })
+        .catch(error => console.error("No se pudo acceder a la api", error))
+}
+
+function editarLibro() {
+    let id = document.getElementById('idEditar').value;
+
+    let editarLibro = {
+        id: document.getElementById('idEditar').value,
+        nombre: document.getElementById('nombreEditar').value,
+        resenia: document.getElementById('reseniaEditar').value,
+        dondeConseguirlo: document.getElementById('dondeConseguirloEditar').value,
+        
+    }
+    fetch(`https://localhost:7153/api/Libros/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(editarLibro)
+    }
+    )
+        .then(() => {
+            document.getElementById('idEditar').value = 0;
+            document.getElementById('nombreEditar').value = "";
+            document.getElementById('reseniaEditar').value = "";
+            document.getElementById('dondeConseguirloEditar').value = "";
+            $('#editarLibro').modal('hide');
+            traerLibros();
+        })
+        .catch(error => console.error("No se pudo editar el elemento", error))
 }
